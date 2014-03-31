523d5ddb816fff5657ef3f5de44e1e604809b1d9
Daniel McDonald <mcdonadt@colorado.edu>
2014-03-31 07:12:52 -0600

    name fixes, minor code cleanup on reroot

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,7 +2,7 @@
 
 from cogent.util.unit_test import TestCase, main
 from t2t.util import combine_alignments, reroot
-from cogent.parse.tree import DndParser
+from skbio.core.tree import TreeNode
 
 __author__ = "Daniel McDonald"
 __copyright__ = "Copyright 2011, The tax2tree project"
@@ -21,7 +21,7 @@ class UtilTests(TestCase):
         """Combine alignments, raise if intersecting ids"""
         lines1 = ['>a','AATTGGCC','>b','AATTAATT']
         lines2 = ['>c','AATTAGCC','>d','AATTGATT']
-        exp = {'a':'AATTGGCC','b':'AATTAATT', 
+        exp = {'a':'AATTGGCC','b':'AATTAATT',
                'c':'AATTAGCC','d':'AATTGATT'}
         obs = combine_alignments(lines1, lines2)
         self.assertEqual(obs, exp)
@@ -29,18 +29,19 @@ class UtilTests(TestCase):
         lines1 = ['>a','AATTGGCC','>b','AATTAATT']
         lines2 = ['>a','AATTAACC','>C','AATTGATT']
         self.assertRaises(ValueError, combine_alignments, lines1, lines2)
-        
+
     def test_reroot(self):
         """Should correctly reroot a tree"""
-        t = DndParser("(((a,b)c,(d,e)f)g,(h,i)j);")
+        t = TreeNode.from_newick("(((a,b)c,(d,e)f)g,(h,i)j);")
         tips = ['a','b']
         for n in t.traverse():
             n.Length = 1.0
-        
+
         # note, g is lost because it has a single descendent and gets pruned off
         exp = "((a:1.0,b:1.0)c:0.5,((d:1.0,e:1.0)f:1.0,(h:1.0,i:1.0)j:2.0):0.5);"
+        exp = "((a,b)c,((d,e)f,(h,i)j));"
         obs = reroot(t, tips)
-        self.assertEqual(obs.getNewick(with_distances=True), exp)
+        self.assertEqual(obs.to_newick(), exp)
 
 if __name__ == '__main__':
     main()
