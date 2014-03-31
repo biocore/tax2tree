ba269aedbdc74e90f1fd0673a19728da36d318ac
Daniel McDonald <mcdonadt@colorado.edu>
2014-03-31 07:33:52 -0600

    setting BSD

--- tests/test_nlevel.py
+++ tests/test_nlevel.py
@@ -10,7 +10,7 @@ in mind.
 __author__ = "Daniel McDonald"
 __copyright__ = "Copyright 2011, The tax2tree project"
 __credits__ = ["Daniel McDonald"]
-__license__ = "GPL"
+__license__ = "BSD"
 __version__ = "1.0"
 __maintainer__ = "Daniel McDonald"
 __email__ = "mcdonadt@colorado.edu"
--- tests/test_util.py
+++ tests/test_util.py
@@ -7,7 +7,7 @@ from skbio.core.tree import TreeNode
 __author__ = "Daniel McDonald"
 __copyright__ = "Copyright 2011, The tax2tree project"
 __credits__ = ["Daniel McDonald"]
-__license__ = "GPL"
+__license__ = "BSD"
 __version__ = "1.0"
 __maintainer__ = "Daniel McDonald"
 __email__ = "mcdonadt@colorado.edu"
