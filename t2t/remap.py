7268c85de85df0f828b0c14ae019df081f3e87f6
Daniel McDonald <mcdonadt@colorado.edu>
2014-04-30 21:11:31 -0600

    ENH: more things

--- t2t/remap.py
+++ t2t/remap.py
@@ -11,3 +11,28 @@ def parse_otu_map(lines):
         res[rep] = members
     return res
 
+def members_to_rep(otus):
+    """Provides a lookup for a cluster member to its rep"""
+    res = {}
+    for rep, members in otus.iteritems():
+        for member in members:
+            res[member] = rep
+    return res
+
+def remap_taxonomy(mapping, taxa):
+    """Remaps the taxonomy over the OTU clusters"""
+    res = {}
+    reps = members_to_rep(mapping)
+
+    for tax_id, tax_str in taxa.iteritems():
+        rep = reps.get(tax_id, None)
+
+        if rep is None:
+            res[tax_id] = tax_str
+            continue
+
+        for m in mapping[rep]:
+            if m in res:
+                print "%s was: %s\nnow is %s" % (m, mapping[m], tax_str)
+            res[m] = tax_str
+    return res
--- tests/test_remap.py
+++ tests/test_remap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from t2t.remap import parse_otu_map
+from t2t.remap import parse_otu_map, members_to_rep, remap_taxonomy
 from unittest import TestCase, main
 
 
@@ -15,6 +15,32 @@ class RemapTests(TestCase):
         obs = parse_otu_map(mapping.splitlines())
         self.assertEqual(obs, exp)
 
+    def test_members_to_rep(self):
+        otus = {"2": ["2", "3", "4"],
+                "5": ["5", "6"],
+                "7": ["7"]}
+        exp = {'2': '2', '3': '2', '4': '2', '5': '5', '6': '5',
+               '7': '7'}
+        obs = members_to_rep(otus)
+        self.assertEqual(obs, exp)
+
+    def test_remap_taxonomy(self):
+        mapping= {"2": ["2", "3", "4"],
+                  "5": ["5", "6"],
+                  "7": ["7"]}
+
+        tax = {"3": ["a", "b", "c"],
+               "7": ["x", "y", "z"],
+               "8": ['foo', 'bar']}
+
+        exp = {"2": ["a", "b", "c"],
+               "3": ["a", "b", "c"],
+               "4": ["a", "b", "c"],
+               "7": ["x", "y", "z"],
+               "8": ['foo', 'bar']}
+
+        obs = remap_taxonomy(mapping, tax)
+        self.assertEqual(obs, exp)
 
 if __name__ == '__main__':
     main()
